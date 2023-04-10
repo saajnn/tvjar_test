ef0f181e04fe2c0f24c007239e72280614f5a5ad
asters1 <gj744559140@163.com>
2023-04-10 08:47:59 +0000

    Test.java

--- app/src/main/java/com/github/catvod/spider/Test.java
+++ app/src/main/java/com/github/catvod/spider/Test.java
@@ -58,9 +58,9 @@ public class Test extends Spider {
             } catch (Exception unused) {
             }
             test0.put("type_id", "0");
-            test0.put("type_name", d);
+            test0.put("type_name", "测试1:" + d);
             test1.put("type_id", "1");
-            test0.put("type_name", e);
+            test0.put("type_name", "测试二:" + e);
 
             classes.put(test0);
             classes.put(test1);
